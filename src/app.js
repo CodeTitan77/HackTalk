ccc19d27c63435048032a05e9dc49309f03ff7c7
Your Name <you@example.com>
2025-06-04 23:32:35 +0530

    Database created

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
  const express=require("express");
  const app= express();
- app.use((req,res)=>{
+ app.use((req,res)=>{  
     res.send("Hello from server")
  })
- app.listen(3000,()=>{
-    console.log("Server started")
- });  
+ app.use("/test",(req,res)=>{
+   res.send("request call from server")
+
+ })
+  app.listen(7777,()=>{
+   console.log("server started")
+  })
+  
+
