f64936e679669b56671c374e441671844a5178da
Your Name <you@example.com>
2025-06-05 13:47:20 +0530

    Data Sanitization done

--- src/app.js
+++ src/app.js
@@ -5,22 +5,81 @@ const express= require("express");
 const app= express();
 const User= require("./models/user");
 
+app.use(express.json());
+
 app.post("/signup",async (req,res)=>{ 
-    const userObj={
-        firstName:"Himanshu",
-        lastName:"Saini",
-        emailId:"Himanshu@gmail.com",
-        password:"hit"
-    }
+   const userObj= req.body;
+   
     
+   try{
     const user= new User(userObj); 
     await user.save();
     res.send("User added successfully") 
+    } 
+    catch(err){
+        res.status(400).send("Error saving the user",err.message);
+    }
 
 
-}
+})
+app.get("/user",async(req,res)=>{
+    const userEmail=req.body.emailId;
+  
+    try{
+    const obj= await User.find({emailId:userEmail})
+    if(obj.length===0){
+        res.status(404).send("User not found");
+    }
+    
+    res.send(obj)
+    }
+    catch(err){
+        res.status(400).send("User not found");
 
-)
+    }
+
+
+})
+app.get("/feed",async(req,res)=>{
+    try{
+    const obj= await User.find();
+    res.send(obj);
+    }
+    catch(err){
+        res.status(400).send("User not found");
+
+    }
+})
+app.delete("/user",async(req,res)=>{
+    const userId=req.body.userId;
+    
+    try{
+        const user=await User.findByIdAndDelete(userId);
+        res.send("User deleted successfully");
+
+    }
+    catch(err){
+         res.status(400).send("Something went wrong");
+    }
+
+})
+app.patch("/user",async(req,res)=>{
+     const userId=req.body._id;
+     const data= req.body;
+    
+    
+    try{
+        const user=await User.findByIdAndUpdate(userId,data);
+        res.send("User updated successfully");
+
+    }
+    catch(err){
+         res.status(400).send("Something went wrong");
+    }
+
+})
+
+ 
 
 connectDb().then(()=>{
     console.log("Database connection established")
--- src/models/user.js
+++ src/models/user.js
@@ -2,22 +2,48 @@ const mongoose= require("mongoose");
 const userSchema=  new mongoose.Schema({
     firstName: {
         type:String,
+        required:true,
+        maxLength:50,
     },
     lastName:{
          type:String,
+         
     },
     emailId:{
         type:String,
+        required:true,
+        lowercase:true,
+        unique:true,
     },
     password:{
         type:String,
+        required:true,
     },
     age:{
-        type:Number
+        type:Number,
+        min:18,
+       
     } ,
      gender:{
-        type:String
-    } 
+        type:String,
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+                throw new Error("Gender not valid")
+            }
+        }
+    }, 
+    photoUrl:{
+        type:String,
+        default:"https://geographyandyou.com/images/user-profile.png"
+
+    },
+    about:{
+        type:String,
+        default:"This is a default about of the user!",
+    },
+    skills:{
+        type:[String],
+    },
 });
 const User= mongoose.model("User",userSchema );
 module.exports=User;
