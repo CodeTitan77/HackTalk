57105d20f362397d3b505f2d9f9266fbee97cb98
Himanshu Trivedi <himanshutrivedi421@gmail.com>
2025-08-10 20:29:29 +0530

    Db query done

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -24,6 +24,15 @@ const connectionRequestSchema= new mongoose.Schema({
 
 
 );
+connectionRequestSchema.pre("save",function(next){
+    const connectionRequest=this;
+    if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)){
+        throw new Error("Cannot send connection request to yourself");
+        
+    }
+    next();
+})//will be called before saving in db will server as 
+connectionRequestSchema.index({ fromUserId: 1,toUserId:1});
 const ConnectionRequestModel = new mongoose.model(
     "ConnectionRequest",
     connectionRequestSchema);
--- src/routes/request.js
+++ src/routes/request.js
@@ -9,6 +9,30 @@ requestRouter.post('/request/send/:status/:toUserId',userAuth,async(req,res)=>{
         const fromUserId=req.user._id;
         const toUserId= req.params.toUserId;
         const status= req.params.status;
+        const allowedStatus=["rejected","accepted"];
+        if(allowedStatus.includes(status)){
+            return res.status(400).json({message:"Invalid status type:"+status})
+        }
+        const toUser= await User.findById(fromUserId);
+        if(!toUser){
+            return res.status(404).json({message:"User not found"})
+        }
+        
+        //check if connection request is already there 
+        const existingConnectionRequest= await ConnectionRequest.findOne({
+            $or:[
+            {fromUserId:fromUserId,
+            toUserId:toUserId},
+           {fromUserId:toUserId,
+            toUserId:fromUserId}
+
+            ],
+           
+        });
+        if(existingConnectionRequest){
+               return res.status(400).json({message:"Connection already exist:"});
+
+        }
         const connectionRequest=new ConnectionRequest({
             fromUserId,
             toUserId,
@@ -23,6 +47,7 @@ requestRouter.post('/request/send/:status/:toUserId',userAuth,async(req,res)=>{
 
     }
     catch(error){
+        res.status(400).json({message:"Error in connecting request"});
 
     }
    
