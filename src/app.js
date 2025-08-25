70384d832b233a91f37f5727cb1b4c2fe5e656f6
Himanshu Trivedi <himanshutrivedi421@gmail.com>
2025-08-26 04:07:52 +0530

    Update authMiddleware

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const {validateSignUpData}= require("./utils/validation")
 const bcrypt= require("bcrypt");
 const validator=require("validator");
 const cookieParser= require("cookie-parser");//test backend
+const cors=require("cors");
 
 const jwt= require("jsonwebtoken");
 const {userAuth} =require("./middlewares/auth")
@@ -12,7 +13,10 @@ const {userAuth} =require("./middlewares/auth")
 const express= require("express");
 const app= express();
 const User= require("./models/user");
-
+app.use(cors({
+      origin:"http://localhost:5173",
+        credentials:true,
+    }));
 app.use(express.json());
 app.use(cookieParser());
 const authRouter=require("./routes/auth");
