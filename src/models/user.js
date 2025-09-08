cb1f7b9c91f9015e779265074a44c19166740acc
Himanshu Trivedi <himanshutrivedi421@gmail.com>
2025-09-08 12:25:22 +0530

    edit in schema

--- src/models/user.js
+++ src/models/user.js
@@ -41,7 +41,7 @@ const userSchema=  new mongoose.Schema({
      gender:{
         type:String,
         validate(value){
-            if(!["male","female","others"].includes(value)){
+            if(!["male","Male","Female","female","others"].includes(value)){
                 throw new Error("Gender not valid")
             }
         }
