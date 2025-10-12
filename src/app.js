f71b945651f21622fd2388f07549fc2f10350422
Himanshu Trivedi <himanshutrivedi421@gmail.com>
2025-10-12 19:28:45 +0530

    debugged stupid error

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ const express= require("express");
 const app= express();
 const User= require("./models/user");
 const chatRouter = require("./routes/chat");
-const paymentRouter = require("./routes/payment");
+// const paymentRouter = require("./routes/payment");
 app.use(cors({
       origin:"http://localhost:5173",
         credentials:true,
@@ -29,12 +29,12 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
- 
+ app.use("/",authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/",userRouter);
 app.use("/",chatRouter);
-app.use("/", paymentRouter);
+// app.use("/", paymentRouter);
 const server= http.createServer(app);
 
 initializeSocket(server);
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -2,27 +2,28 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 const userAuth = async (req, res, next) => {
-    try {
-        const {token} = req.cookies;
-        if(!token) {
-            return res.status(401).send("Please Login!"); 
-        }
-        
-        const decodedObj = await jwt.verify(token, process.env.JWT_SECRET);
-        const {_id} = decodedObj;
-        const user = await User.findById(_id);
-        
-        if(!user) {
-            throw new Error("User Not Found");
-        }
-        
-        req.user = user;
-        next(); 
-    } catch(err) {
-        return res.status(400).send("Error: " + err.message); 
+  try {
+    const { token } = req.cookies;
+
+    if (!token) {
+      return res.status(401).send("Please Login!");
     }
-}
+
+    const decodedObj = jwt.verify(token, process.env.JWT_SECRET);
+    const { _id } = decodedObj;
+
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(401).send("User not found.");
+    }
+
+    req.user = user;
+    next();
+  } catch (err) {
+    return res.status(401).send("Unauthorized: " + err.message);
+  }
+};
 
 module.exports = {
-    userAuth,
-}
+  userAuth,
+};
